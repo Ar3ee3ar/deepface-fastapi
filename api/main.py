1dddcb8a6dfd630b04d85351dbd5be7e214765ee
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2023-10-15 20:42:03 +0700

    prod

--- api/main.py
+++ api/main.py
@@ -1,5 +1,12 @@
-# run api : uvicorn test-api:app --reload
-from typing import List, Annotated
+import asyncio
+from http import HTTPStatus
+from typing import Dict, List
+from uuid import UUID, uuid4
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from deepface import DeepFace
+
 from starlette.responses import StreamingResponse
 import cv2
 import io
@@ -8,15 +15,63 @@ import base64
 from PIL import Image
 import json
 import os
+from annoy import AnnoyIndex
+from tqdm import tqdm
+import time
 
-from fastapi import FastAPI, File, UploadFile, Body
+from fastapi import FastAPI, File, UploadFile, Body, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 from pydantic import BaseModel
 
 # from .face_aging_standalone import main_mani
 # import face_aging_standalone
-from .process.verify import search
+from .process.verify import filter, get_all, filter_id, filter_greater_than
+# from .process.annoy_process import u
+
+u = AnnoyIndex(512, 'angular')
+
+def create_annoy():
+    # a.unload()
+    # try:
+    global u
+    embeded_list = []
+    pass_list = []
+    print('load image...')
+    dir_list = get_all()
+
+    for i in tqdm(range(len(dir_list))):
+        name_pic_file = dir_list[i][1].split('/')
+        # file_type = name_pic_file[-1].split('.')
+        name_path = '/var/www/html/pic/'+name_pic_file[-1]
+        if(os.path.exists(name_path) and name_path.find('.jpg') != -1):
+            try:
+                embedding_objs = DeepFace.represent(img_path = name_path, model_name= "Facenet512",enforce_detection= False)
+                embeded_list.append(embedding_objs[0])
+                pass_list.append(dir_list[i][0])
+            except cv2.error as e:
+                print("Custom error message: Unable to decode image.")
+                pass
+    f = len(embeded_list[0]["embedding"])  # Length of item vector that will be indexed
+
+    u = AnnoyIndex(f, 'angular')
+    u.on_disk_build('db_face.ann')
+
+    print('create index...')
+    for i in tqdm(range(len(embeded_list))):
+        v = embeded_list[i]["embedding"]
+        u.add_item(pass_list[i], v)
+
+    u.build(10) # 10 trees
+    # print(dir_list[len(dir_list)-1])
+    with open('update_date.txt', 'w') as f:
+        f.write(str(dir_list[len(dir_list)-1][2]))
+    print('Finish')
+    return True
+    # except:
+    #     return False
+create_annoy()
+# u.load('db_face.ann') # super fast, will just mmap the file
 
 class Req_search(BaseModel):
     file_path: str
@@ -28,78 +83,223 @@ class Res_search_value(BaseModel):
     similarity: float
 
 class Res_search_main(BaseModel):
-    value: list[Res_search_value]
+    value: List[Res_search_value]
+
+class Job(BaseModel):
+    uid: UUID = Field(default_factory=uuid4)
+    status: str = ""
+    progress: int = 0
+    all_img: int = 0
+    result: List[Res_search_value] = [{"id":0,"similarity":0.0}]
 
 app = FastAPI()
+jobs: Dict[UUID, Job] = {}  # Dict as job storage
+# all_result = {"value":[]}
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1"
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+def long_task(all_result: List, db_list:List, search_pic:str):
+    # all_result = {"value":[]}
+    all_time_verify = 0
+    found_pic = []
+    for i in range(len(db_list)):
+        name_pic_file = db_list[i][1].split('/')
+        # file_type = name_pic_file[-1].split('.')
+        name_path = '/var/www/html/pic/'+name_pic_file[-1]
+        if(os.path.exists(name_path) and name_path.find('.jpg') != -1):
+            try:
+                result = DeepFace.verify(img1_path = search_pic, img2_path = name_path ,enforce_detection= False, model_name= "Facenet512", detector_backend ='opencv')
+                # print(str(result['verified']),' => ',str(result['distance']))
+                if(result['verified'] and result['distance'] < 0.35):
+                        # all_result.append(dir_list[i][1])
+                        # all_time_verify = all_time_verify + int(result["time"])
+                        # pic = rect_pic(dir_list[i][2],result['facial_areas']['img2'])
+                        # found_pic.append(pic)
+                        json_pic = {
+                            "id":db_list[i][0],
+                            "similarity": (1-result['distance'])
+                        }
+                        all_result["value"].append(json_pic)
+            except cv2.error as e:
+                # print("OpenCV Error:", e)
+                print("Custom error message: Unable to decode image.")
+                pass
+    return all_result
+    #         jobs[task_id].progress = i+1
+    #     jobs[task_id].result = all_result
+    # jobs[task_id].status = "completed"
+    # for i in range(1, param):  # do work and return our progress
+    #     await asyncio.sleep(1)
+    #     await queue.put(i)
+    # await queue.put(None)
 
-@app.get("/")
-async def hello():
-    return {"message":"Face verification api"}
 
-# @app.get("/test")
-# async def hello():
-#     return {"message":"OK"}
+# async def start_new_task(uid: UUID, data: Req_search) -> None:
 
-# @app.post("/files")
-# async def create_file(file: Annotated[bytes, File()]):
-#     return {"file_size": len(file)}
+    
 
+    # queue = asyncio.Queue()
+    # task = asyncio.create_task(long_task(queue, start_dt =str(data.start_datetime),end_dt=str(data.end_datetime),search_pic=data.file_path))
+
+    # print(queue.get())
+    # while progress := await queue.get():  # monitor task progress
+    #     jobs[uid].progress = progress
+    # jobs[uid].result = all_result
+    # jobs[uid].status = "complete"
+
+
+# @app.post("/search_deep", status_code=HTTPStatus.ACCEPTED)
+# async def task_handler(background_tasks: BackgroundTasks, data: Req_search) -> Job:
+#     new_task = Job()
+#     jobs[new_task.uid] = new_task
+#     dir_list = filter(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+#     jobs[new_task.uid].all_img = len(dir_list)
+#     jobs[new_task.uid].status = "in_progress"
+#     all_result = []
+#     background_tasks.add_task(long_task,all_result, new_task.uid,db_list = dir_list,search_pic=data.file_path)
+#     return new_task
+
+
+@app.get("/task/{uid}/status")
+async def status_handler(uid: UUID) -> Job:
+    return jobs[uid]
+
+@app.get("/create_tree")
+def recreate_annoy():
+    flag = create_annoy()
+    if(flag):
+        return {"message": "Finish create data for face verification"}
+    else:
+        return {"message": "Error"}
+
+# @app.post("/search_tree", status_code=HTTPStatus.ACCEPTED)
+# async def task_handler_tree(background_tasks: BackgroundTasks, data: Req_search) -> Job:
+#     new_task = Job()
+#     jobs[new_task.uid] = new_task
+#     dir_list = filter_id(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+#     jobs[new_task.uid].all_img = len(dir_list)
+#     jobs[new_task.uid].status = "in_progress"
+#     all_result = []
+#     background_tasks.add_task(search_tree,all_result, new_task.uid,db_list = dir_list,search_pic=data.file_path)
+#     return new_task
+
+
+
+def search_tree(all_result: List, db_list:List, search_pic:str):
+    # all_result = []
+    # dir_list = filter_id(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+    embedding_objs = DeepFace.represent(img_path = search_pic, model_name= "Facenet512",enforce_detection=False, detector_backend ='opencv')
+    rank = u.get_nns_by_vector(embedding_objs[0]["embedding"], u.get_n_items(), include_distances=True)
+    rank_id = rank[0]
+    rank_sim = rank[1]
+    db_list_id = [i[0] for i in db_list]
+    # print('db_list_id: ',db_list_id)
+    # print('tree: ',rank_id)
+
+    i = 0
+    for id,sim in zip(rank_id,rank_sim):
+        if(id in db_list_id and 1-((sim**2)/2) > 0.65):
+            # print('id: ',id,' => sim: ',1-((sim**2)/2))
+            json_pic = {
+                            "id":id,
+                            "similarity": 1-((sim**2)/2)
+                        }
+            all_result["value"].append(json_pic)
+        i = i +1
+    #     jobs[task_id].progress = i
+    #     jobs[task_id].result = all_result
+    # # jobs[task_id].progress = len(db_list_id)
+    # jobs[task_id].status = "completed"
+    return all_result
 
 @app.post("/verify")
-async def create_upload_file(data: Req_search) -> Res_search_main:
-    # print(file.filename)
-    # contents = await file.read()
-    # image_np = np.frombuffer(contents, np.uint8)
-    # img_np = cv2.imdecode(image_np, cv2.IMREAD_COLOR) 
-    # print(str(start_datetime))
-    # print(str(end_datetime))
-    # data = req.json() # change input(request model) to Json
-    # data = json.loads(data) # change Json to Dict
-    if(os.path.exists(data.file_path)):
-        result = search(start_dt = str(data.start_datetime),end_dt=str(data.end_datetime),search_pic=data.file_path)
-        return result
-    else:
-        return {'message': 'Confirm that '+ data.file_path +' exists'}
-    # return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+def task_handler_combine(data: Req_search) -> Res_search_main:
+    flag_new = False
+    f = open("update_date.txt", "r")
+    date_db = f.readline()
+    f.close() 
 
-# @app.post("/uploadfiles")
-# async def create_upload_file(files: List[UploadFile]):
-#     # print(len(file))
-#     pic_list =[]
-#     name_list = []
-#     pic_in=[]
-#     name_in=[]
-#     for file in files:
-#         contents = await file.read()
-#         image_np = np.frombuffer(contents, np.uint8)
-#         img_np = cv2.imdecode(image_np, cv2.IMREAD_COLOR) 
-#         img_pil = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-#         # print(type(img_np))
-#         pic_in.append(Image.fromarray(img_pil))
-#         name_img_list  = file.filename.split('.')
-#         name_img = name_img_list[0]
-#         name_in.append(name_img)
-#         # gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-#         # # img = cv2.imread(img_np) 
-#         # res, im_png = cv2.imencode(".png", gray)
-#         # name_img_list  = file.filename.split('.')
-#         # name_img = name_img_list[0]+'_gray'+'.png'
-#         # name_list.append(name_img)
-#         # pic_list.append(base64.b64encode(im_png.tobytes()))
-#     # pic_list,name_list = face_aging_standalone.main_mani(pic_in,name_in)
-#     return {"name":name_list,"pic": pic_list}
+    time_db_dt = datetime.strptime(date_db,'%Y-%m-%d %H:%M:%S')
+    time_start_dt = datetime.strptime(str(data.start_datetime),'%Y-%m-%d %H:%M:%S')
+    time_end_dt = datetime.strptime(str(data.end_datetime),'%Y-%m-%d %H:%M:%S')
+
+    # new_task = Job()
+    # jobs[new_task.uid] = new_task
+    # jobs[new_task.uid].status = "in_progress"
+    all_result = {"value":[]}
+    # search data newer than in (annoy) index data -> deepface
+    if(time_start_dt > time_db_dt):
+        print('deepface')
+        dir_list = filter(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+        # jobs[new_task.uid].all_img = len(dir_list)
+        result = long_task(all_result,db_list = dir_list,search_pic=data.file_path)
+    # search data in (annoy) index data with some newer data -> deepface + annoyindex
+    elif(time_start_dt < time_db_dt and time_end_dt > time_db_dt):
+        print('deepface + annoyindex')
+        dir_list = filter(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+        db_above = filter(start_dt=str(time_db_dt + timedelta(seconds=1)),end_dt=str(data.end_datetime))
+        # jobs[new_task.uid].all_img = len(dir_list)
+        if(len(db_above) > 0):
+            # print(db_above)
+            # print('have more data')
+            flag_new = True
+        result = combine_process(
+                                  all_result,
+                                  db_list = dir_list,
+                                  search_pic=data.file_path,
+                                  flag_new = flag_new,
+                                  db_new = db_above)
+    # sear data in (annoy) index data -> annoyindex
+    elif(time_start_dt <= time_db_dt and time_end_dt <= time_db_dt):
+        print('annoyindex')
+        dir_list = filter(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+        # jobs[new_task.uid].all_img = len(dir_list)
+        result = search_tree(all_result, db_list = dir_list,search_pic=data.file_path)
+    return result
+
+
+def combine_process(all_result: List, db_list:List, search_pic:str, flag_new: bool, db_new:List):
+    # all_result = []
+    # dir_list = filter_id(start_dt=str(data.start_datetime),end_dt=str(data.end_datetime))
+    embedding_objs = DeepFace.represent(img_path = search_pic, model_name= "Facenet512",enforce_detection=False)
+    rank = u.get_nns_by_vector(embedding_objs[0]["embedding"], u.get_n_items(), include_distances=True)
+    rank_id = rank[0]
+    rank_sim = rank[1]
+    db_list_id = [i[0] for i in db_list]
+
+    count = 0
+    for id,sim in zip(rank_id,rank_sim):
+        # print(count)
+        if(id in db_list_id and (1-((sim**2)/2)) > 0.65):
+            # print('id: ',id,' => sim: ',1-((sim**2)/2))
+            json_pic = {
+                            "id":id,
+                            "similarity": (1-((sim**2)/2))
+                        }
+            all_result["value"].append(json_pic)
+        # count = count +1
+        # jobs[task_id].progress = count
+        # jobs[task_id].result = all_result
+    # count = len(db_list_id)
+    # jobs[task_id].progress = len(db_list_id)
+    
+    if(flag_new):
+        for i in range(len(db_new)):
+            name_pic_file = db_new[i][1].split('/')
+            # print('/var/www/html/pic/'+name_pic_file[-1])
+            name_path = '/var/www/html/pic/'+name_pic_file[-1]
+            if(os.path.exists(name_path) and name_path.find('.jpg') != -1):
+                try:
+                    result = DeepFace.verify(img1_path = search_pic, img2_path = name_path ,enforce_detection= False, model_name= "Facenet512", detector_backend ='opencv')
+                    # print(str(result['verified']),' => ',str(result['distance']))
+                    if(result['verified'] and result['distance'] < 0.35):
+                            json_pic = {
+                                "id":db_new[i][0],
+                                "similarity": (1-result['distance'])
+                            }
+                            all_result["value"].append(json_pic)
+                            # print(json_pic)
+                except cv2.error as e:
+                    # print("OpenCV Error:", e)
+                    print("Custom error message: Unable to decode image.")
+                    pass
+    return all_result
--- api/process/verify.py
+++ api/process/verify.py
@@ -1,5 +1,6 @@
 from deepface import DeepFace
 import os
+from os.path import join, dirname
 # from .connect_db import mydb
 import mysql.connector
 from PIL import Image
@@ -7,78 +8,108 @@ from io import BytesIO
 import base64
 import numpy as np
 import cv2
+from dotenv import dotenv_values,load_dotenv
 # import time
 
-def rect_pic(img,position):
-    img_pil = Image.open(BytesIO(base64.b64decode(img)))
-    img_cv = cv2.cvtColor(np.array(img_pil), cv2.COLOR_BGR2RGB)
-    start_point = (int(position['x']), int(position['y']))
-    end_point = (int(position['x'] + position['h']), int(position['y'] + position['w']))
-    color = (0, 255, 0)
-    thickness = 2
-    image = cv2.rectangle(img_cv, start_point, end_point, color, thickness)
-    res,img_jpg = cv2.imencode(".jpg",image)
-    return (base64.b64encode(img_jpg.tobytes()))
-
-def pair_verify(search_pic,dir_list):
-    all_result = {
-        "value":[]
-    }
-    all_time_verify = 0
-    found_pic = []
-    for i in range(len(dir_list)):
-        name_pic_file = dir_list[i][1].split('/')
-        # file_type = name_pic_file[-1].split('.')
-        name_path = '/var/www/html/pic/'+name_pic_file[-1]
-        if(os.path.exists(name_path) and name_path.find('.jpg') != -1):
-            # print(dir_list[i][1])
-            try:
-                result = DeepFace.verify(img1_path = search_pic, img2_path = name_path ,enforce_detection= False, model_name= "Facenet", detector_backend ='opencv')
-                if(result['verified'] and result['distance'] < 0.25):
-                        # all_result.append(dir_list[i][1])
-                        # all_time_verify = all_time_verify + int(result["time"])
-                        # pic = rect_pic(dir_list[i][2],result['facial_areas']['img2'])
-                        # found_pic.append(pic)
-                        json_pic = {
-                            "id":dir_list[i][0],
-                            "similarity": (1-result['distance'])
-                        }
-                        # print(type(pic))
-                        # print(type(dir_list[i][3]))
-                        all_result["value"].append(json_pic)
-            except cv2.error as e:
-                # print("OpenCV Error:", e)
-                print("Custom error message: Unable to decode image.")
-                pass
-
-    # # print(search_pic + " is the same as")
-    # for i in range(len(all_result)):
-    #     print(str(i) + '. '+all_result[i])
-
-    return (all_result)
+# def connector():
+#     dotenv_path = join(dirname(__file__), '.env')
+#     load_dotenv(dotenv_path)
+#     mydb = mysql.connector.connect(
+#         # host="host.docker.internal",
+#         # host="192.168.1.119",
+#         host = os.environ.get('HOST_db'),
+#         user=os.environ.get('USER_db'),
+#         password=os.environ.get('PASSWORD_db'),
+#         database=os.environ.get('DATABASE_db'),
+#         port = os.environ.get('PORT_db')
+#     )
+#     # mycursor = mydb.cursor()
+#     return mydb
 
 def filter(start_dt,end_dt):
     # date format y/m/d
     # start_dt = 2008-01-01 00:00:00
     # end_dt = 2008-01-10 00:00:00
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
     mydb = mysql.connector.connect(
         # host="host.docker.internal",
         # host="192.168.1.119",
-        host = "192.168.1.119",
-        user="exat",
-        password="Pwd123456!",
-        database="center",
-        port = 3306
+        host = os.environ.get('HOST_db'),
+        user=os.environ.get('USER_db'),
+        password=os.environ.get('PASSWORD_db'),
+        database=os.environ.get('DATABASE_db'),
+        port = os.environ.get('PORT_db')
     )
     mycursor = mydb.cursor()
     mycursor.execute("SELECT id,pic_path, create_time FROM kiosk_robot_snap WHERE create_time BETWEEN '"+start_dt+"' AND '"+end_dt+"' order by create_time")
-    # mycursor.execute("SELECT name,PhotoB64, Dt_create FROM testDB_verify WHERE Dt_create BETWEEN '"+start_dt+"' AND '"+end_dt+"'")
+    # mycursor.execute("SELECT ID,PicPath, Dt_create FROM pic WHERE Dt_create BETWEEN '"+start_dt+"' AND '"+end_dt+"' ORDER BY Dt_create")
     myresult = mycursor.fetchall()
     # print(myresult)
     return myresult
 
-def search (start_dt,end_dt,search_pic):
-    img_db = filter(start_dt,end_dt)
-    # print(img_db)
-    all_result = pair_verify(search_pic,img_db) # 0-name, 1-pic path, 2-create_time
-    return (all_result)
+def filter_id(start_dt,end_dt):
+    # date format y/m/d
+    # start_dt = 2008-01-01 00:00:00
+    # end_dt = 2008-01-10 00:00:00
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    mydb = mysql.connector.connect(
+        # host="host.docker.internal",
+        # host="192.168.1.119",
+        host = os.environ.get('HOST_db'),
+        user=os.environ.get('USER_db'),
+        password=os.environ.get('PASSWORD_db'),
+        database=os.environ.get('DATABASE_db'),
+        port = os.environ.get('PORT_db')
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT id FROM kiosk_robot_snap WHERE create_time BETWEEN '"+start_dt+"' AND '"+end_dt+"' order by create_time")
+    # mycursor.execute("SELECT ID FROM pic WHERE Dt_create BETWEEN '"+start_dt+"' AND '"+end_dt+"' ORDER BY Dt_create")
+    myresult = mycursor.fetchall()
+    # print(myresult)
+    return myresult
+
+def get_all():
+    # date format y/m/d
+    # start_dt = 2008-01-01 00:00:00
+    # end_dt = 2008-01-10 00:00:00
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    mydb = mysql.connector.connect(
+        # host="host.docker.internal",
+        # host="192.168.1.119",
+        host = os.environ.get('HOST_db'),
+        user=os.environ.get('USER_db'),
+        password=os.environ.get('PASSWORD_db'),
+        database=os.environ.get('DATABASE_db'),
+        port = os.environ.get('PORT_db')
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT id,pic_path, create_time FROM kiosk_robot_snap order by create_time")
+    # mycursor.execute("SELECT ID,PicPath,Dt_create FROM pic ORDER BY Dt_create")
+    myresult = mycursor.fetchall()
+    # print(myresult)
+    return myresult
+
+def filter_greater_than(store_date):
+    # date format y/m/d
+    # start_dt = 2008-01-01 00:00:00
+    # end_dt = 2008-01-10 00:00:00
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    mydb = mysql.connector.connect(
+        # host="host.docker.internal",
+        # host="192.168.1.119",
+        host = os.environ.get('HOST_db'),
+        user=os.environ.get('USER_db'),
+        password=os.environ.get('PASSWORD_db'),
+        database=os.environ.get('DATABASE_db'),
+        port = os.environ.get('PORT_db')
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT id,pic_path, create_time FROM kiosk_robot_snap WHERE create_time > '"+store_date+"' order by create_time")
+    # mycursor.execute("SELECT ID FROM pic WHERE Dt_create > '"+store_date+"' ORDER BY Dt_create")
+    myresult = mycursor.fetchall()
+    # print(myresult)
+    return myresult
